1813113f1924c4a15f41f172eaecfd248c3d985a
Marcella Chaves <127541337+marcellafccosta@users.noreply.github.com>
2025-11-11 11:02:24 -0300

    Add files via upload

---
+++ code/server/src/modules/users/users.module.ts
@@ -0,0 +1,18 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersRepository } from '../../shared/repositories/users.repository';
+import { User } from '../../shared/entities/user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { ApartamentoModule } from '../../modules/apartamento/apartamento.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => ApartamentoModule),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersRepository, UsersService, TypeOrmModule],
+})
+export class UsersModule {}
